d66903028d1303bb109f68213aad2d62ef90be63
ovays62 <beshear1986@gmail.com>
2026-08-07 17:41:35 +0330

    Update page.js

--- app/builder/page.js
+++ app/builder/page.js
@@ -1,40 +1,61 @@
+"use client";
+
+
+import {useState} from "react";
+
 import FoodCanvas from "@/components/builder/FoodCanvas";
+
+import IngredientPanel 
+from "@/components/builder/IngredientPanel";
 
 
 export default function Builder(){
 
+
+const [ingredients,setIngredients]
+=
+useState([]);
+
+
 return (
 
-<main
-className="
+<main className="
 min-h-screen
 pt-32
 px-5
 galaxy-bg
-"
->
+">
 
 
-<h1
-className="
+<h1 className="
 text-5xl
 font-black
 gradient-text
 text-center
 mb-10
-"
->
+">
 
 ساخت غذای من 🍕
 
 </h1>
 
 
-<FoodCanvas/>
+<FoodCanvas
+ingredients={ingredients}
+/>
+
+
+<IngredientPanel
+
+selected={ingredients}
+
+setSelected={setIngredients}
+
+/>
 
 
 </main>
 
 )
 
-}
+  }
