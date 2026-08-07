094ea4169a7216cc858402ddca9b67c5f2fe0254
ovays62 <beshear1986@gmail.com>
2026-08-07 17:07:52 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  output: "export",
   images: {
-
-    unoptimized:true
-
-  }
-
+    unoptimized: true,
+  },
 };
-
 
 module.exports = nextConfig;
