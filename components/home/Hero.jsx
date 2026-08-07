7bfea71af57f5cb7ac28642fdbb541a8220453c2
ovays62 <beshear1986@gmail.com>
2026-08-07 17:33:42 +0330

    Update Hero.jsx

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
+
 
 export default function Hero(){
 
@@ -20,43 +21,84 @@ px-5
 
 
 <motion.h1
-initial={{opacity:0,y:40}}
-animate={{opacity:1,y:0}}
+
+initial={{
+opacity:0,
+scale:.8
+}}
+
+animate={{
+opacity:1,
+scale:1
+}}
+
+transition={{
+duration:1
+}}
+
 className="
 text-5xl
-md:text-7xl
+md:text-8xl
 font-black
 gradient-text
-">
+"
 
-غذای آینده را
+>
+
+غذای آینده
 <br/>
-خودت بساز
+را بساز
 
 </motion.h1>
 
 
+
 <motion.p
-initial={{opacity:0}}
-animate={{opacity:1}}
-transition={{delay:.4}}
+
+initial={{
+opacity:0
+}}
+
+animate={{
+opacity:1
+}}
+
+transition={{
+delay:.5
+}}
+
 className="
-mt-6
+mt-8
 text-xl
 text-white/70
-">
+"
 
-با OmFood غذا را طراحی کن،
-بساز و سفارش بده 🚀
+>
+
+یک تجربه فست‌فود سه‌بعدی،
+هوشمند و شخصی‌سازی شده
 
 </motion.p>
 
 
-<div className="mt-10 flex gap-5 justify-center">
 
-<button className="btn-primary">
+<div className="
+flex
+justify-center
+gap-5
+mt-10
+">
+
+
+<button className="
+btn-primary
+text-lg
+">
+
 🍕 ساخت غذای من
+
 </button>
+
 
 
 <button className="
@@ -65,8 +107,11 @@ px-8
 py-4
 rounded-2xl
 ">
+
 مشاهده منو
+
 </button>
+
 
 </div>
 
